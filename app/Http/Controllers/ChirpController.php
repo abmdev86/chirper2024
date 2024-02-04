94de49e7db34c04f231fa258f4b8178fe86df39d
Auroiah Morgan <amwebdev@outlook.com>
2024-02-04 10:24:15 -0500

    Update ChirpController.php

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -62,9 +62,19 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        // ensure the authorized user is updating.
+        $this->authorize('update', $chirp);
+        $validated = $request->validate([
+            'message'=> 'required|string|max:255'
+        ]);
+
+        
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
+
     }
 
     /**
